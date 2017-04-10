ffb1826d0b554c1bf4303eb93c3d7cdb39e1d582
YASSINE SABRI <yassine.sabri@edu.uca.ma>
2017-04-10 03:03:23 +0100

    change to angular-cli build

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,15 +1,14 @@
-import { browser, element, by } from 'protractor';
+import { NewAppPage } from './app.po';
 
-describe('QuickStart E2E Tests', function () {
+describe('new-app App', () => {
+  let page: NewAppPage;
 
-  let expectedMsg = 'Hello Angular';
-
-  beforeEach(function () {
-    browser.get('');
+  beforeEach(() => {
+    page = new NewAppPage();
   });
 
-  it('should display: ' + expectedMsg, function () {
-    expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
   });
-
 });
